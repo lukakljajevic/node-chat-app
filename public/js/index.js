cb69c7c33844135386ea3029100c89ceae8f38e1
Luka Kljajevic <luka.kljajevic.dev@gmail.com>
2019-02-25 10:08:07 +0100

    Format timestamps using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -10,16 +10,18 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
+  let formattedTime = moment(message.createdAt).format('h:mm a');
   let li = $('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`(${formattedTime}) ${message.from}: ${message.text}`);
   $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
+  let formattedTime = moment(message.createdAt).format('h:mm a');
   let li = $('<li></li>');
   let a = $('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`(${formattedTime}) ${message.from}: `);
   a.attr('href', message.url);
   li.append(a);
   $('#messages').append(li);
@@ -53,7 +55,7 @@ locationButton.click(function() {
       alert('Unable to fetch location');
       locationButton.removeAttr('disabled').text('Send location');
     });
-  }, 2000);
+  }, 1500);
   
   
 
