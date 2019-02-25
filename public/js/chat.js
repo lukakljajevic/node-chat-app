a00e93bdec3d8bfa7687c4b85d7240d2dd09af5c
Luka Kljajevic <luka.kljajevic.dev@gmail.com>
2019-02-25 16:31:14 +0100

    Send messages to only people in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -69,7 +69,6 @@ $('#message-form').on('submit', function(e) {
   e.preventDefault();
   let messageTextbox = $('input[name="message"]');
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function() {
     messageTextbox.val('');
